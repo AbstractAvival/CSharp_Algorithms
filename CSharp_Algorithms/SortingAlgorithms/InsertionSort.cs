0bd9714f5f4cbc387c45295f2b7899241286035d
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-12 23:48:05 -0500

    Add project files.

---
+++ CSharp_Algorithms/SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿/***************************************
+Insertion Sort:
+
+A simple sorting algorithm that builds the final sorted array
+one item at a time. It's efficient when sorting a small number
+of elements.
+***************************************/
+
+namespace CSharp_Algorithms.SortingAlgorithms
+{
+	public class InsertionSort
+	{
+		public int [] Sort( int [] array )
+		{
+			int key, currentItem;
+			for( int index = 1; index < array.Length; index++ )
+			{
+				key = array[ index ];
+				currentItem = index - 1;
+				while( currentItem > -1 && array[ currentItem ] > key )
+				{
+					array[ currentItem + 1 ] = array[ currentItem ];
+					currentItem--;
+				}
+				array[ currentItem + 1 ] = key;
+			}
+			return array;
+		}
+	}
+}
