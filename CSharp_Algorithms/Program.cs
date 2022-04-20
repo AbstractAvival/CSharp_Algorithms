580e2933758e9acd1fd40d75fb40b4cbdc4927e5
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-20 18:27:21 -0500

    Implemented Selection Sort.

--- CSharp_Algorithms/Program.cs
+++ CSharp_Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSharp_Algorithms.SortingAlgorithms;
 
 namespace CSharp_Algorithms
 {
@@ -7,6 +8,22 @@ namespace CSharp_Algorithms
 		static void Main( string [] args )
 		{
 			Console.WriteLine( "Hello Algorithms!" );
+			SelectionSort sorter = new SelectionSort();
+
+			int [] values = { 84, 55, 12, 110, 37, 41, 74, 9, 62, 29 };
+
+			foreach( int number in values )
+			{
+				Console.WriteLine( number );
+			}
+
+			Console.WriteLine( "\n" );
+			sorter.Sort( values );
+
+			foreach( int number in values )
+			{
+				Console.WriteLine( number );
+			}
 		}
 	}
 }
---
+++ CSharp_Algorithms/SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿/***************************************
+Selection Sort:
+
+Sorts an array by repeatedly finding the minimum element
+(or largest, depending on sorting order) in the unsorted list,
+swapping it with the leftmost unsorted element, and moving
+the sublist boundaries one element to the right.
+
+Similiar to Insertion Sort, but generally performs worse.
+***************************************/
+
+namespace CSharp_Algorithms.SortingAlgorithms
+{
+	public class SelectionSort
+	{
+		public void Sort( int [] array )
+		{
+			for( int currentIndex = 0; currentIndex < array.Length - 1; currentIndex++ )
+			{
+				int minimumIndex = currentIndex;
+				int offsetIndex = minimumIndex + 1;
+				while( offsetIndex < array.Length )
+				{
+					if( array[ offsetIndex ] < array[ minimumIndex ] )
+					{
+						minimumIndex = offsetIndex;
+					}
+					offsetIndex++;
+				}
+				int valueholder = array[ currentIndex ];
+				array[ currentIndex ] = array[ minimumIndex ];
+				array[ minimumIndex ] = valueholder;
+			}
+		}
+	}
+}
