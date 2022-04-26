10ae35920d863695e6e47ea8a29238cb6d341941
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-26 15:47:44 -0500

    Created class ShellSort

---
+++ CSharp_Algorithms/SortingAlgorithms/ShellSort.cs
@@ -0,0 +1,19 @@
+﻿/***************************************
+Shell Sort:
+
+An improved insertion sort. Starts by sorting pairs of
+elements far apart from each other, then progressively
+reduces the gap between elements to be compared. Running 
+time is heavily dependent on the gap sequence used.
+***************************************/
+
+namespace CSharp_Algorithms.SortingAlgorithms
+{
+	public class ShellSort
+	{
+		public void Sort( int [] array )
+		{
+
+		}
+	}
+}
