3677ee8c7050f18139a61f935da321a3bc8c3312
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-29 08:14:33 -0500

    Created MergeSort class.

---
+++ CSharp_Algorithms/SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,16 @@
+﻿/***************************************
+Merge Sort:
+
+An efficient, general-purpose, and comparison-based sorting
+algorithm. Merge sort is a divide and conquer algorithm. It 
+divides the input array into halves until it has n sublists,
+then it repeatedly merges these sublists untill there is only
+one sublist remaining. This final sublist is the sorted list.
+***************************************/
+
+namespace CSharp_Algorithms.SortingAlgorithms
+{
+	public class MergeSort
+	{
+	}
+}
