48d4b7075beed76764ea2aa86c5df8329159a464
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-30 16:20:50 -0500

    Fixed typo in comment.

--- CSharp_Algorithms/SortingAlgorithms/MergeSort.cs
+++ CSharp_Algorithms/SortingAlgorithms/MergeSort.cs
@@ -4,7 +4,7 @@ Merge Sort:
 An efficient, general-purpose, and comparison-based sorting
 algorithm. Merge sort is a divide and conquer algorithm. It 
 divides the input array into halves until it has n sublists,
-then it repeatedly merges these sublists untill there is only
+then it repeatedly merges these sublists until there is only
 one sublist remaining. This final sublist is the sorted list.
 ***************************************/
 
