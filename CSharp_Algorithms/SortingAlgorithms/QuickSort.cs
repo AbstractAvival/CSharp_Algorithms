8ada4d50805d86a24fde6761c636c7bd182757a9
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-03 15:17:24 -0500

    Created QuickSort class.

---
+++ CSharp_Algorithms/SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,29 @@
+﻿/***************************************
+Quick Sort:
+
+A divide and conquer algorithm. It picks an element as
+a pivot point and partitions the given array around
+said pivot into sub-arrays. The sub-arrays are then 
+sorted recursively.
+***************************************/
+
+namespace CSharp_Algorithms.SortingAlgorithms
+{
+	public class QuickSort
+	{
+		public void Sort( int [] array, int startIndex, int endIndex )
+		{
+			if( startIndex >= endIndex )
+				return;
+
+			int partitionIndex = Partition( array, startIndex, endIndex );
+			Sort( array, startIndex, partitionIndex - 1 );
+			Sort( array, partitionIndex + 1, endIndex );
+		}
+
+		private int Partition( int [] array, int startIndex, int endIndex )
+		{
+			return -1;
+		}
+	}
+}
