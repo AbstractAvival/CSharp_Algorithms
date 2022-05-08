984c34e77e41ebba75ef2fa62b10ecb59d375d19
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-08 14:27:54 -0500

    Removed unused code.

--- CSharp_Algorithms/Program.cs
+++ CSharp_Algorithms/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CSharp_Algorithms.SortingAlgorithms;
 
 namespace CSharp_Algorithms
 {
@@ -8,15 +7,6 @@ namespace CSharp_Algorithms
 		static void Main( string [] args )
 		{
 			Console.WriteLine( "Hello Algorithms!" );
-			int [] test = { 327, 547, 689, 0, 12, 1, 333, 998, 0, 12, 32 };
-
-			BubbleSort sorter = new BubbleSort();
-			sorter.Sort( test );
-
-			foreach( int number in test )
-			{
-				Console.WriteLine( number );
-			}
 		}
 	}
 }
