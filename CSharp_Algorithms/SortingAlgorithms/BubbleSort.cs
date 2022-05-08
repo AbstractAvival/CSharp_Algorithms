fef5b1bfc9f02475abde0a55adbddefd804bc2cd
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-08 14:10:28 -0500

    Added comments to BubbleSort class.

--- CSharp_Algorithms/SortingAlgorithms/BubbleSort.cs
+++ CSharp_Algorithms/SortingAlgorithms/BubbleSort.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/***************************************
+Bubble Sort:
+
+A sorting algorithm that compares two adjacent elements and 
+swaps them if they are in the wrong order. This is repeated 
+until the list is sorted.
+***************************************/
 
 namespace CSharp_Algorithms.SortingAlgorithms
 {
 	public class BubbleSort
 	{
-		public void Sort( )
+		public void Sort( int [] array )
 		{
+			bool swappedElements = false;
+			do 
+			{
+				swappedElements = false;
+				for( int currentIndex = 0; currentIndex < array.Length - 1; currentIndex++ )
+				{
+					if( array[ currentIndex ] > array[ currentIndex + 1 ] )
+					{
 
+					}
+				}
+			} while( swappedElements );
 		}
 	}
 }
